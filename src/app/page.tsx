6cf6ff98960001888c1f39279a94369490354b95
Robert Gleason <l2ob122@gmail.com>
2023-11-25 16:53:49 -0800

    added next link to link between the pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
+import Link from 'next/link';
+
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      <h1>Home</h1>
+      <ul>
+        <li>
+          <Link href='/performance'>
+            Performance
+          </Link>
+        </li>
+        <li>
+          <Link href='/reliability'>
+            Reliability
+          </Link>
+        </li>
+        <li>
+          <Link href='/scale'>
+            Scale
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
